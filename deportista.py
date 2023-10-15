51892ca733d12d9594011dc33ba6f7dcc8c306de
RBucheli8 <141966641+RBucheli8@users.noreply.github.com>
2023-10-15 03:47:22 -0500

    Primer commit

---
+++ deportista.py
@@ -0,0 +1,16 @@
+class Deportista:
+    def __init__(self, años=0):
+        self._deporte = "Futbol"
+        self._añosPracticando = años
+
+    def getDeporte(self):
+        return self._deporte
+
+    def setDeporte(self, deporte):
+        self._deporte = deporte
+
+    def getAñosPracticando(self):
+        return self._añosPracticando
+
+    def setAñosPracticando(self, años):
+        self._añosPracticando = años
---
+++ futbolista.py
@@ -0,0 +1,43 @@
+from persona import Persona
+from deportista import Deportista
+
+
+class Futbolista(Persona, Deportista):
+    _listaFutbolistas = []
+
+    def __init__(self, nombre="", edad=0, altura=0, sexo="M", años=0, goles=0, tarjetas=0, pierna=""):
+        Persona.__init__(nombre, edad, altura, sexo)
+        Deportista.__init__(años)
+        self._golesMarcados = goles
+        self._tarjetasRojas = tarjetas
+        self._piernaHabil = pierna
+        Futbolista._listaFutbolistas.append(self)
+
+    def getGolesMarcados(self):
+        return self._golesMarcados
+
+    def setGolesMarcados(self, goles):
+        self._golesMarcados = goles
+
+    def getTarjetasRojas(self):
+        return self._tarjetasRojas
+
+    def setTarjetasRojas(self, tarjetas):
+        self._tarjetasRojas = tarjetas
+
+    def getPiernaHabil(self):
+        return self._piernaHabil
+
+    def setPiernaHabil(self, pierna):
+        self._piernaHabil = pierna
+
+    @classmethod
+    def getListaFutbolistas(self):
+        return Futbolista._listaFutbolistas
+
+    @classmethod
+    def setListaFutbolistas(self, lista):
+        Futbolista._listaFutbolistas = lista
+
+    def __str__(self):
+        return f"Mi nombre es {self.getNombre} soy profesional en el deporte {self.getDeporte} Tengo {self.getEdad} años de edad y llevo {self.getAñosPracticando} años en el deporte"
